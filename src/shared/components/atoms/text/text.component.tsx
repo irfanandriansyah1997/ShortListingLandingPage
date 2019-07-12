b369406a95c6aefe1049f7229daaa186b48d47c5
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-07-12 10:36:53 +0700

    feat(component): create styling for text component

--- src/shared/components/atoms/text/text.component.tsx
+++ src/shared/components/atoms/text/text.component.tsx
@@ -8,8 +8,9 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { convert_obj_to_string } from '@/shared/utils/string.utils';
-
 import { PropsInterface } from './interfaces/component.interface';
+
+import './style/style.scss';
 
 class Text extends React.Component<PropsInterface> {
     static propTypes = {
@@ -21,16 +22,20 @@ class Text extends React.Component<PropsInterface> {
             PropTypes.node,
             PropTypes.string
         ]).isRequired,
-        align: PropTypes.oneOf(['center', 'left', 'right', 'initial'])
+        align: PropTypes.oneOf(['center', 'left', 'right', 'initial']),
+        name: PropTypes.string
     };
 
     static defaultProps = {
         styling: 'text',
-        align: 'initial'
+        align: 'initial',
+        name: ''
     };
 
     get className(): string {
-        const { styling, fontWeight, align } = this.props;
+        const {
+            styling, fontWeight, align, name
+        } = this.props;
         const FONT_WEIGHT = {
             300: 'light',
             400: 'normal',
@@ -40,9 +45,11 @@ class Text extends React.Component<PropsInterface> {
         };
 
         return convert_obj_to_string({
-            [`ui-atomic-icon--style-${styling}`]: true,
-            [`ui-atomic-icon--font-weight-${FONT_WEIGHT[fontWeight]}`]: true,
-            [`ui-atomic-icon--align-${align}`]: true
+            [`${name}`]: true,
+            'ui-atomic-text': true,
+            [`ui-atomic-text--style-${styling}`]: true,
+            [`ui-atomic-text--font-weight-${FONT_WEIGHT[fontWeight]}`]: true,
+            [`ui-atomic-text--align-${align}`]: true
         });
     }
 
