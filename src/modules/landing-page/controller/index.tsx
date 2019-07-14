5c9a2807404bd93648a8ade70cc84e89b408235c
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-07-14 14:09:22 +0700

    feat(mvvm-pattern-setup): setup provider & view model

---
+++ src/modules/landing-page/controller/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Props } from '@/modules/landing-page/interfaces/viewmodel.interface';
+
+class LandingPageControler extends React.Component<Props> {
+    render(): React.ReactNode {
+        return <div>Ini Controler</div>;
+    }
+}
+
+export default LandingPageControler;
--- src/modules/landing-page/index.tsx
+++ src/modules/landing-page/index.tsx
@@ -1,55 +1,11 @@
 import * as React from 'react';
-import { DefaultPropsInterface } from '@/shared/interfaces/component.interface';
-import { ListingInterface, ListingList } from '@/modules/landing-page/interfaces/listing.interface';
-import Shortlist from '@/modules/landing-page/components/molecules/shortlist/shortlist.component';
-import Header from '@/shared/components/molecules/header/header.component';
+import ViewModel from './view-model';
+import Controller from './controller';
 
-interface StateComponent {
-    model: ListingInterface[];
-}
-
-class LandingPageModule extends React.Component<DefaultPropsInterface, StateComponent> {
-    getPropertyData(): ListingList {
-        return {
-            count: 2,
-            properties: [
-                {
-                    description: 'string',
-                    id: 1,
-                    listingType: 1,
-                    location: 'string',
-                    marketType: 1,
-                    picture: 'string',
-                    propertyType: 1,
-                    rentType: 1,
-                    status: 1,
-                    title: 'string'
-                },
-                {
-                    description: 'string',
-                    id: 1,
-                    listingType: 1,
-                    location: 'string',
-                    marketType: 1,
-                    picture: 'string',
-                    propertyType: 1,
-                    rentType: 1,
-                    status: 2,
-                    title: 'string'
-                }
-            ]
-        };
-    }
-
+class LandingPageModule extends React.Component {
     render() {
-        return (
-            <div className="ui-landing-page-module">
-                <Header title="Listing 500 Juta" />
-                <div className="ui-landing-page-module__content">
-                    <Shortlist propertyData={this.getPropertyData()} />
-                </div>
-            </div>
-        );
+        const Module = ViewModel(Controller);
+        return <Module />;
     }
 }
 
