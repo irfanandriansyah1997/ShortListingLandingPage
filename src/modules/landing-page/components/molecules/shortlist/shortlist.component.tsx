d7e22dc8784fc21cdb5d9100cda3f61ed657851e
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-07-15 22:53:53 +0700

    feat(irfan-landing-page): create contact agent component

--- src/modules/landing-page/components/molecules/shortlist/shortlist.component.tsx
+++ src/modules/landing-page/components/molecules/shortlist/shortlist.component.tsx
@@ -1,29 +1,54 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { ListingListInterface } from '@/store/listing/interfaces/listing.interface';
+import { ListingInterface } from '@/store/listing/interfaces/listing.interface';
+import Text from '@/shared/components/atoms/text/text.component';
+import ContactAgent from '@/modules/landing-page/components/molecules/contact-agent/contact-agent.component';
 
 import './style.scss';
 
 interface ShortlistProps {
-    propertyData: ListingListInterface;
+    listing: ListingInterface;
 }
 
 class Shortlist extends React.Component<ShortlistProps> {
     static propTypes = {
-        propertyData: PropTypes.shape({
-            count: PropTypes.number,
-            properties: PropTypes.arrayOf(PropTypes.shape({}))
-        }).isRequired
+        listing: PropTypes.shape({}).isRequired
     };
 
     render() {
-        const { propertyData } = this.props;
+        const { listing } = this.props;
 
         return (
-            <div className="shortlist">
-                <div className="shortlist__sidebar">{propertyData.count}</div>
-                <hr />
-                <div className="shortlist__detail">a</div>
+            <div className="ui-molecules-shortlist flex relative">
+                <Text name="ui-molecules-shortlist__id" tag="p" styling="text" fontWeight={500}>
+                    {listing.id}
+                </Text>
+                <Text
+                    name="ui-molecules-shortlist__price"
+                    tag="h3"
+                    styling="subheading"
+                    fontWeight={600}
+                >
+                    {listing.attribute.priceTag}
+                </Text>
+                <Text
+                    name="ui-molecules-shortlist__title"
+                    tag="h3"
+                    styling="subheading"
+                    fontWeight={600}
+                >
+                    {listing.title}
+                </Text>
+                <Text
+                    name="ui-molecules-shortlist__location"
+                    tag="h3"
+                    styling="subheading"
+                    fontWeight={600}
+                >
+                    {listing.location}
+                    {listing.agent.contact.telephone}
+                </Text>
+                <ContactAgent photo={listing.agent.picture} name={listing.agent.name} />
             </div>
         );
     }
