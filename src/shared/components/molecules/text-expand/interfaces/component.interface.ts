56dd8255a6d63800e21a9b3d851ccfd7c338f143
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-07-16 15:14:28 +0700

    feat(irfan-landing-page): create text-expand component

---
+++ src/shared/components/molecules/text-expand/interfaces/component.interface.ts
@@ -0,0 +1,6 @@
+import { DefaultPropsInterface } from '@/shared/interfaces/component.interface';
+
+export interface PropsInterface extends DefaultPropsInterface {
+    content: string;
+    title: string;
+}
